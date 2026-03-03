4a4c0ed252f1e89fd8c7a63de747b4999baf7481
dinakar-ss <ds7090@srmist.edu.in>
2026-03-03 11:20:00 +0530

    UC2

---
+++ OOPSBannerApp.java
@@ -0,0 +1,40 @@
+
+/**
+ * OOPS Banner App
+ *
+ * UC2: Render OOPS as Banner using Print Statements
+ *
+ * @author YourName
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        printOOPSBanner();
+    }
+
+    static void printOOPSBanner() {
+
+        System.out.println(
+            "*********" + "  *********" + "  *********" + "  *********"
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *       *" + "  *        "
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *       *" + "  *        "
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *********" + "  *********"
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *          " + "        *  "
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *          " + "        *  "
+        );
+        System.out.println(
+            "*********" + "  *********" + "  *          " + "*********"
+        );
+    }
+}
