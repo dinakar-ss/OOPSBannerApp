44ce1306355381361f8b761af1a378dd4a2ba0e8
dinakar-ss <ds7090@srmist.edu.in>
2026-03-11 11:29:28 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,33 +1,62 @@
-
+import java.util.HashMap;
 
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
-        printOOPSBanner();
+    public static HashMap<Character, String[]> buildCharacterPatterns() {
+
+        HashMap<Character, String[]> patterns = new HashMap<>();
+
+        patterns.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patterns.put('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        patterns.put('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        });
+
+        return patterns;
     }
 
-    static void printOOPSBanner() {
+    public static void printBanner(String word, HashMap<Character, String[]> patterns) {
 
-        System.out.println(
-            "*********" + "  *********" + "  *********" + "  *********"
-        );
-        System.out.println(
-            "*       *" + "  *       *" + "  *       *" + "  *        "
-        );
-        System.out.println(
-            "*       *" + "  *       *" + "  *       *" + "  *        "
-        );
-        System.out.println(
-            "*       *" + "  *       *" + "  *********" + "  *********"
-        );
-        System.out.println(
-            "*       *" + "  *       *" + "  *          " + "        *  "
-        );
-        System.out.println(
-            "*       *" + "  *       *" + "  *          " + "        *  "
-        );
-        System.out.println(
-            "*********" + "  *********" + "  *          " + "*********"
-        );
+        for (int i = 0; i < 7; i++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                String[] pattern = patterns.get(ch);
+                line.append(pattern[i]).append("  ");
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HashMap<Character, String[]> patterns = buildCharacterPatterns();
+
+        printBanner("OOPS", patterns);
     }
 }
