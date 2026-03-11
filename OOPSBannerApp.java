06a1937b4669ba1255cfd44f99f207dd83235e7f
dinakar-ss <ds7090@srmist.edu.in>
2026-03-11 11:22:56 +0530

    commit the UC8

---
+++ OOPSBannerApp.java
@@ -0,0 +1,33 @@
+
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        printOOPSBanner();
+    }
+
+    static void printOOPSBanner() {
+
+        System.out.println(
+            "*********" + "  *********" + "  *********" + "  *********"
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *       *" + "  *        "
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *       *" + "  *        "
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *********" + "  *********"
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *          " + "        *  "
+        );
+        System.out.println(
+            "*       *" + "  *       *" + "  *          " + "        *  "
+        );
+        System.out.println(
+            "*********" + "  *********" + "  *          " + "*********"
+        );
+    }
+}
